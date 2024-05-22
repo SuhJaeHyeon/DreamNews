4d341fc6b77f1965d56669c87c0963a3c3518231
jaehyeons <marindec16@gmail.com>
2024-05-22 18:59:57 +0700

    Initial Commit

---
+++ DreamMegazin/DreamMegazinApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamMegazinApp.swift
+//  DreamMegazin
+//
+//  Created by JaehyeonS on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DreamMegazinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
